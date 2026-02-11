9fd223657e9e40ee4b4917d6d626ca43638542ac
CRS <yadukrishna2240@gmail.com>
2026-02-11 21:00:38 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "GetX Demo",
+      title: "ayurvedic project",
       home: SplashPage(),
     );
   }
